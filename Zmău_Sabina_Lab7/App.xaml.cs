cf45306a9b6a576c8f526dcbd2a631cf098dc621
Sabina Zmău <sabina.zmau.sz@gmail.com>
2022-12-06 15:05:04 +0200

    lab8

--- Zmău_Sabina_Lab7/App.xaml.cs
+++ Zmău_Sabina_Lab7/App.xaml.cs
@@ -1,8 +1,21 @@
-﻿namespace Zmău_Sabina_Lab7;
+﻿using System;
+using Zmău_Sabina_Lab7.Data;
+using System.IO;
+
+namespace Zmău_Sabina_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database { 
+		get { 
+			if (database == null) 
+			
+			{ database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+			} 
+			return database; } 
+	}
+    public App()
 	{
 		InitializeComponent();
 
---
+++ Zmău_Sabina_Lab7/ListPage.xaml.cs
@@ -0,0 +1,20 @@
+using Zmău_Sabina_Lab7.Models;
+
+namespace Zmău_Sabina_Lab7;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e) { 
+		var slist = (ShopList)BindingContext; 
+		slist.Date = DateTime.UtcNow; 
+		await App.Database.SaveShopListAsync(slist); 
+		await Navigation.PopAsync(); }
+    async void OnDeleteButtonClicked(object sender, EventArgs e) { 
+		var slist = (ShopList)BindingContext; 
+		await App.Database.DeleteShopListAsync(slist); 
+		await Navigation.PopAsync(); }
+}
